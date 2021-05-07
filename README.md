66aa92cede5870bf5a88a084e7635184f867081b
dezhizhang <1541609448@qq.com>
2021-05-08 07:14:58 +0800

    update

--- README.md
+++ README.md
@@ -230,3 +230,43 @@ let option = {
 myCharts.setOption(option);
 ```
 ![image.png](https://i.loli.net/2021/05/08/L5NCOV6qtyDFjkT.png)
+### 通用配置legend
+```
+import echarts from 'echarts';
+let root = document.getElementById('root');
+let myCharts = echarts.init(root);
+let option = {
+    
+    xAxis:{
+        type:'category',
+        data:['张三','李四','王五','马六','小明','二妞','大强'],
+    },
+    yAxis:{
+        type:'value'
+    },
+    legend:{
+        data:['语文','数学']
+    },
+    series:[
+        {   
+            name:'语文',
+            type:'bar',
+            label:{
+                show:true,
+                position:'top',
+            },
+            barWidth:'40%',
+            data:[88,92,63,77,94,80,72,]
+        },
+        {
+            name:'数学',
+            type:'bar',
+            barWidth:'40%',
+            data:[88,22,44,77,94,80,72,]
+        }
+    ]
+}
+myCharts.setOption(option);
+
+```
+![image.png](https://i.loli.net/2021/05/08/nrVWFuSD6evdfjG.png)
--- example/bar6.js
+++ example/bar6.js
@@ -2,16 +2,32 @@ import echarts from 'echarts';
 let root = document.getElementById('root');
 let myCharts = echarts.init(root);
 let option = {
-    
+    toolbox:{
+        feature:{
+            saveAsImage:{ //导出图片
+                show:true,
+            },
+            dataView:{ //数据视图
+                show:true,
+            },
+            restore:{ //数据重置
+                show:true,
+            },
+            dataZoom:{//区域缩放
+                show:true,
+            },
+            magicType:{ //动态图表类形切换
+                type:['bar','line']
+            }
+
+        }
+    },
     xAxis:{
         type:'category',
         data:['张三','李四','王五','马六','小明','二妞','大强'],
     },
     yAxis:{
         type:'value'
-    },
-    legend:{
-        data:['语文','数学']
     },
     series:[
         {   
@@ -23,12 +39,6 @@ let option = {
             },
             barWidth:'40%',
             data:[88,92,63,77,94,80,72,]
-        },
-        {
-            name:'数学',
-            type:'bar',
-            barWidth:'40%',
-            data:[88,22,44,77,94,80,72,]
         }
     ]
 }
